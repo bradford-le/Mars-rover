a4ab6b616ab497d12d06f042085dea9a04e26270
Brad Le <brad.le444@gmail.com>
2017-06-10 22:37:02 +0200

    GUI changes

--- mars_rover/rover.js
+++ mars_rover/rover.js
@@ -1,5 +1,5 @@
 var myRover = {
-  position: [0,0], 
+  position: [0,0],
   direction: 'N'
 };
 
@@ -18,8 +18,70 @@ function goForward(rover) {
       rover.position[1]--
       break;
   };
+console.log("New Rover Position: [" + rover.position[0] + ", " + rover.position[1] + "]")
+}
 
-  console.log("New Rover Position: [" + rover.position[0] + ", " + rover.position[1] + "]")
+function goBackward(rover) {
+  switch (rover.direction) {
+    case 'N':
+      rover.position[0]--
+      break;
+    case 'E':
+      rover.position[1]--
+      break;
+    case 'S':
+      rover.position[0]++
+      break;
+    case 'W':
+      rover.position[1]++
+      break;
+  };
+console.log("New Rover Position: [" + rover.position[0] + ", " + rover.position[1] + "]")
+}
+
+function goLeft(rover) {
+  switch (rover.direction) {
+    case 'N':
+      rover.direction = "W";
+      break;
+    case 'E':
+      rover.direction = "N";
+      break;
+    case 'S':
+      rover.direction = "E";
+      break;
+    case 'W':
+      rover.direction = "S";
+      break;
+  };
+console.log("New Rover Position: [" + rover.position[0] + ", " + rover.position[1] + "]")
+}
+
+function goRight(rover) {
+  switch (rover.direction) {
+    case 'N':
+			rover.direction = 'E';
+			break;
+		case 'E':
+			rover.direction = 'S';
+			break;
+		case 'S':
+			rover.direction = 'W';
+			break;
+		case 'W':
+			rover.direction = 'N';
+			break;
+  };
+console.log("New Rover Position: [" + rover.position[0] + ", " + rover.position[1] + "]")
 }
 
 goForward(myRover);
+goBackward(myRover);
+goBackward(myRover);
+
+// JQUERY
+$(document).ready(function() {
+$("#forward").click(function() {
+  $("#input-box:text").val("f");
+});
+})
